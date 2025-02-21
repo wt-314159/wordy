73baf437cc54b7456a0d2ceee4880c2c46ed0728
William Train <William.Train@ametek.com>
2025-02-21 10:26:24 +0000

    Using command arguments to choose the algorithm implementation

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,38 @@ struct Args {
     /// Number of games to play.
     #[arg(short, long)]
     games: Option<usize>,
+
+    #[arg(value_enum)]
+    algorithm: Algorithm
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, ValueEnum)]
+enum Algorithm {
+    /// Chooses the option that gives us the most information.
+    Entropy,
+    /// Chooses the most common word that matches the history of guesses.
+    Naive
 }
 
 fn main() {
     let args = Args::parse();
     let num_games = args.games.unwrap_or(usize::MAX);
+    match dbg!(args.algorithm) {
+        Algorithm::Entropy => play(wordy::algorithms::Entropy::new, num_games),
+        Algorithm::Naive => play(wordy::algorithms::Naive::new, num_games)
+    }
+}
+
+fn play<S>(mut maker: impl FnMut() -> S, games: usize) 
+where 
+    S: wordy::Solver,
+{
     let mut scores = Vec::new();
     let mut failed = 0;
     let mut over_6 = 0;
-    for answer in ANSWERS.lines().take(num_games) {
-        if let Some(score) = wordy::Wordle::play(answer, wordy::algorithms::Entropy::new(), 32) {
+    let mut game_count = 0;
+    for answer in ANSWERS.lines().take(games) {
+        if let Some(score) = wordy::Wordle::play(answer, maker(), 32) {
             println!("Guessed {} in {}", answer, score);
             scores.push(score);
             if score > 6 {
@@ -27,10 +49,11 @@ fn main() {
             println!("Failed to guess {}", answer);
             failed += 1;
         }
+        game_count +=1;
     }
-    let average = scores.iter().sum::<usize>() as f64 / (num_games - failed) as f64;
+    let average = dbg!(scores.iter().sum::<usize>()) as f64 / dbg!((game_count - failed)) as f64;
     println!(
-        "Average score: {:02}, Failed to solve: {}, Solved in more than 6 guesses: {}",
+        "Average score: {:.2}, Failed to solve: {}, Solved in more than 6 guesses: {}",
         average, failed, over_6
     );
 }
